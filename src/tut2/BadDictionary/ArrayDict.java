604d30265fd5f2a6c5337897da291c2b09494302
TheBarzani <ismaelmergasori@gmail.com>
2023-05-17 12:33:40 -0400

    added the implementation for a bad dictionary

--- src/tut2/BadDictionary/ArrayDict.java
+++ src/tut2/BadDictionary/ArrayDict.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public class ArrayDict<K, V> {
 
-    ArrayList<K>[] key;
-    ArrayList<V>[] val;
+    ArrayList<K> key;
+    ArrayList<V> val;
     int size;
     int curr;
 
     ArrayDict(int s){
         size = s;
-        key = new ArrayList[size];
-        val = new ArrayList[size];
+        key = new ArrayList<>();
+        val = new ArrayList<>();
         curr = 0;
     }
 
     public Integer find(K k){
         Integer i;
         for (i=0; i < curr; i++){
-            if(key[i] == k) return i;
+            if(key.get(i) == k) return i;
         }
         return curr;
     }
@@ -27,36 +27,37 @@ public class ArrayDict<K, V> {
     public V get(K k){
         Integer index = find(k);
         if (index == null) return null;
-        else return val[index];
+        else return val.get(index);
     }
 
     public boolean set(K k, V v){
         Integer index = find(k);
         if (index == null) return false;
         else{
-            val[index] = v;
+            val.set(index, v);
             return true;
         }
     }
 
     public boolean add(K k, V v){
-        Integer index = find(k);
-        if (index != null || curr == size) return false;
+        if (curr != 0 && (curr == size)) return false;
         else{
-            key[curr] = k;
-            val[curr] = v;
+            key.add(curr,k);
+            val.add(curr,v);
             curr++;
             return true;
         }
     }
 
     public static void main(String[] args) {
-        ArrayDict<Character, String> myDict = new ArrayDict(4);
+        ArrayDict<Character, String> myDict = new ArrayDict<>(4);
         myDict.add('a', "alpha");
         myDict.add('o', "omega");
         myDict.add('g',"gamma");
 
-        System.out.println(myDict.get(a));
+        System.out.println(myDict.get('a'));
+        System.out.println(myDict.get('o'));
+        System.out.println(myDict.get('g'));
 
     }
     
